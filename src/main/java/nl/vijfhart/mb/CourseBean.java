26a88608dbd669f1c4d581414a7722c7b9258f19
Mark Gortzak <mgortzak@gmail.com>
2017-10-06 14:37:44 +0200

    Now with actual data.

--- src/main/java/nl/vijfhart/mb/CourseBean.java
+++ src/main/java/nl/vijfhart/mb/CourseBean.java
@@ -1,34 +1,20 @@
 package nl.vijfhart.mb;
 
+import nl.vijfhart.controller.CourseController;
 import nl.vijfhart.model.Course;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import java.util.ArrayList;
 import java.util.List;
 
 @ManagedBean
 @RequestScoped
 public class CourseBean {
+    @EJB
+    private CourseController courseController;
 
     public List<Course> getCourses() {
-        List<Course> courses = new ArrayList<>();
-
-        Course foo = new Course();
-        foo.setName("foo");
-        foo.setDescription("foo! :-(");
-        foo.setDuration(5);
-        foo.setPrice(500);
-
-        Course bar = new Course();
-        bar.setName("bar");
-        bar.setDescription("bar! :-(");
-        bar.setDuration(3);
-        bar.setPrice(600);
-
-        courses.add(foo);
-        courses.add(bar);
-
-        return courses;
+        return courseController.findAll();
     }
 }
