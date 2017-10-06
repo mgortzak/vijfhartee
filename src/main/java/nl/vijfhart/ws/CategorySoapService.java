cfe15138dc103f07646931f8c7df645000aac18e
Mark Gortzak <mgortzak@gmail.com>
2017-10-06 09:45:11 +0200

    Added a categories SOAP WS.

---
+++ src/main/java/nl/vijfhart/ws/CategorySoapService.java
@@ -0,0 +1,29 @@
+package nl.vijfhart.ws;
+
+import nl.vijfhart.dao.CategoryDao;
+import nl.vijfhart.model.Category;
+import nl.vijfhart.model.Course;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateless
+@WebService(serviceName = "CategorySoap")
+public class CategorySoapService {
+    @EJB
+    private CategoryDao categoryDao;
+
+    public List<Category> fetchCategories() {
+
+        List<Category> categories = categoryDao.findAll();
+        for (Category category : categories) {
+            for (Course course : category.getCourses()) {
+                course.setCategories(new ArrayList<>());
+            }
+        }
+        return categories;
+    }
+}
