0a4676cc4b4d3a23ee94b8245a1d3911a9c0b7ee
Mark Gortzak <mgortzak@gmail.com>
2017-10-06 09:45:23 +0200

    Now with a nicer service name.

--- src/main/java/nl/vijfhart/ws/CourseSoapService.java
+++ src/main/java/nl/vijfhart/ws/CourseSoapService.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
-@WebService(serviceName = "MyCourses")
+@WebService(serviceName = "CourseSoap")
 public class CourseSoapService {
     @EJB
     private CourseDao courseDao;
